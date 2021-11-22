fa6fe9cfded088db1df76f482eb9a9dfda2f9bd9
Shrivyas-179 <shrivyasrao@gmail.com>
2021-11-22 12:37:20 +0530

    Db not created

--- EmployeeHome/models.py
+++ EmployeeHome/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Employeedb(models.Model):
+    ssn=models.IntegerField(unique=True,max_length=100,null=True)
+    name=models.CharField(max_length=100,null=True)
+    email=models.EmailField(max_length=100,null=True)
+    password=models.CharField(max_length=100,null=True)
+    dob=models.DateField(null=True)
+    city=models.CharField(max_length=100,null=True)
--- EmployeeHome/views.py
+++ EmployeeHome/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect,response
+from .models import Employeedb
+import EmployeeHome
 # Create your views here.
+def register(request):
+    if(request.method=='POST'):
+        ssn=request.POST.get('ssn')
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        dob=request.POST.get('dob')
+        city=request.POST.get('city')
+        user = Employeedb.objects.create(ssn=ssn, name=name, email=email, password=password, dob=dob, city=city)
+        user.save()
+        print("ssn-{}".format(ssn))
+    return render(request, 'EmployeeHome/register.html')
